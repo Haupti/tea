150f300f0022298ae282b9ecc813bb0e3cf27f6d
marwin <marwin.ack@googlemail.com>
2023-11-21 14:01:54 +0100

    update assetion.h so give better output and add test

--- test/assert.h
+++ test/assert.h
@@ -10,10 +10,10 @@
 #define TEST(x,y) { \
     int failure_count = 0;\
     int assertion_count = 0;\
-    printf("    TEST "#x"\n");\
+    char test_name[100] = x;\
     y\
     if(failure_count == 0){\
-        printf("SUCCESSFUL");\
+        printf("    SUCCESSFUL TEST "#x"\n");\
     }\
 };
 
@@ -21,7 +21,7 @@
     assertion_count += 1;\
     if(x!=y){\
         failure_count += 1;\
-        printf("        FAILURE in assertion %d: expected "#x"=="#y", but was not.\n", assertion_count);\
+        printf("    FAILURE IN TEST \"%s\". failed in assertion %d: expected "#x"=="#y", but was not.\n", test_name, assertion_count);\
     }\
 }
 
@@ -29,7 +29,7 @@
     assertion_count += 1;\
     if(strcmp(x, y) != 0){\
         failure_count += 1;\
-        printf("        FAILURE in assertion %d: expected %s==%s, but was not.\n", assertion_count,x,y);\
+        printf("    FAILURE IN TEST \"%s\". failed in assertion %d: expected %s==%s, but was not.\n", test_name, assertion_count,x,y);\
     }\
 }
 
--- test/main.c
+++ test/main.c
@@ -18,4 +18,16 @@ DESCRIBE("read_tokens", {
         show_token(show, &tokens[2]);
         ASSERT_STR_EQUALS(show,"OFF");
     })
+    TEST("reads (1 & 0) | 1)",{
+        TokenType tokens[5];
+        int count = read_tokens(tokens, "(1 & 0) | 1", 11);
+        ASSERT_EQUALS(count,7);
+        char show[20];
+        show_token(show, &tokens[0]);
+        ASSERT_STR_EQUALS(show,"GRP_OPEN");
+        show_token(show, &tokens[4]);
+        ASSERT_STR_EQUALS(show,"GRP_CLOSE");
+        show_token(show, &tokens[5]);
+        ASSERT_STR_EQUALS(show,"OR");
+    })
 })
