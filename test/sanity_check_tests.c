a0816ec31550141b237916b0696f9b3649abf276
marwin <marwin.ack@googlemail.com>
2023-11-29 19:26:42 +0100

    add tests for sanity checker

--- test/sanity_check_tests.c
+++ test/sanity_check_tests.c
@@ -23,4 +23,21 @@ MODULAR_DESCRIBE(sanity_check_tests,{
         ASSERT_INT_EQUALS(check.has_error, 1);
         ASSERT_STR_EQUALS(SANITY_ERROR_TYPE_STR(check.result.error.error_type),SANITY_ERROR_TYPE_STR(SNITY_ERR_NO_OVERWRITE));
     })
+    TEST("checks that assignments dont overwrite existing identifier (not within assignment itself)",{
+        char * a = "a";
+        Token tokens[] = ARRAY(new_token(SET), new_identifier_token(a), new_token(ASSIGNMENT_OPERATOR), new_token(ON), new_token(STATEMENT_END),
+                               new_token(SET), new_identifier_token(a), new_token(ASSIGNMENT_OPERATOR), new_token(ON), new_token(STATEMENT_END),
+                               new_identifier_token(a));
+        SanityCheck check = sanity_check(tokens, 11);
+        ASSERT_INT_EQUALS(check.has_error, 1);
+        ASSERT_STR_EQUALS(SANITY_ERROR_TYPE_STR(check.result.error.error_type),SANITY_ERROR_TYPE_STR(SNITY_ERR_NO_OVERWRITE));
+    })
+    TEST("checks you cannot use an identifier before assignment before",{
+        char * a = "a";
+        Token tokens[] = ARRAY(new_identifier_token(a),
+                               new_token(SET), new_identifier_token(a), new_token(ASSIGNMENT_OPERATOR), new_token(ON), new_token(STATEMENT_END));
+        SanityCheck check = sanity_check(tokens, 6);
+        ASSERT_INT_EQUALS(check.has_error, 1);
+        ASSERT_STR_EQUALS(SANITY_ERROR_TYPE_STR(check.result.error.error_type),SANITY_ERROR_TYPE_STR(SNITY_ERR_USE_BEFORE_ASSIGNMENT));
+    })
 })
